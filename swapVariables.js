d28c006faf0afae958cd28231a8166c9705f2d86
ChatchananM <165884301+ChatchananM@users.noreply.github.com>
2024-05-14 12:22:39 +0700

    First commit for data type Exercise-3

---
+++ swapVariables.js
@@ -0,0 +1,16 @@
+// Assign 1st-values to variables
+let cupFirst = "Americano";
+let cupSecond = "Espresso";
+
+console.log("แก้วแรก before =", cupFirst);
+console.log("แก้วที่สอง before =", cupSecond);
+
+// Swap variables
+let buffer1st, buffer2nd;
+buffer1st = cupFirst;
+buffer2nd = cupSecond;
+//console.log(buffer1st, buffer2nd);
+// cupFirst = buffer2nd;
+// cupSecond = buffer1st;
+console.log("แก้วแรก after =", cupFirst);
+console.log("แก้วที่สอง after =", cupSecond);
